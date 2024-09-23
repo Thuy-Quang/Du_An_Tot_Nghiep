e46395fefafb2ee1a616b63d9d1459ea1ab3a4d9
Thuy_quang <thuylqph39306@fpt.edu.vn>
2024-09-23 15:14:22 +0700

    Initial commit

---
+++ src/test/java/com/example/du_an_tot_nghiep/DuAnTotNghiepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.du_an_tot_nghiep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuAnTotNghiepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
